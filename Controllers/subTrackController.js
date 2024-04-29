fdb9985af680aca88cc463a808980e3e64915441
Mustafa Mahmoud <moustafamaahmoud30@gmail.com>
2024-04-29 20:03:08 +0300

    last commit

--- Controllers/subTrackController.js
+++ Controllers/subTrackController.js
@@ -1,8 +1,7 @@
 const SubTrack = require('../Models/subTrackModel');
 const catchAsync = require('../utilities/catchAsync');
 const AppError = require('../utilities/AppErrors');
-const Book = require('../Models/booksModel');
-const Course = require('../Models/coursesModel');
+
 
 exports.getAllSubTracks = catchAsync(async (req, res, next) => {
   const subTracks = await SubTrack.find({ track: req.track._id });
@@ -16,9 +15,14 @@ exports.getAllSubTracks = catchAsync(async (req, res, next) => {
 });
 
 exports.getSteps = catchAsync(async (req, res, next) => {
-  const steps = await SubTrack.find({ track: req.track._id }).select('name')
+  let query;
+   query = SubTrack.find({ track: req.track._id }).select('name')
     .populate({ path: 'books' })
     .populate({ path: 'courses' });
+  const page=req.query.page *1;
+  const limit=req.query.limit *1;
+  const skip=(page-1)*limit
+  const steps=await query.skip(skip).limit(limit);
   res.status(200).json({
     status: 'success',
     length: steps.length,
