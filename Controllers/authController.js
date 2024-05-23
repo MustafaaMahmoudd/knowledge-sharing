4b7c67fcecd6edc050e35d140e5dd48770ea78ee
Mustafa Mahmoud <moustafamaahmoud30@gmail.com>
2024-05-23 20:50:51 +0300

    last commit

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -21,9 +21,9 @@ const createSendToken = (res, user, statusCode) => {
     expire: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true, //store jwt in the browser and don't modify it
+    // httpOnly: true, //store jwt in the browser and don't modify it
   };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
   user.password = undefined;
   res.cookie('jwt', token, cookieOptions);
   res.status(statusCode).json({
