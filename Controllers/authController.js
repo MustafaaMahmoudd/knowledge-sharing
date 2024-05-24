b5141fba582770f66e58aec7ddfacafd065e5183
Mustafa Mahmoud <moustafamaahmoud30@gmail.com>
2024-05-24 17:37:30 +0300

    last commit

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -139,7 +139,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   } else if (req.cookies.jwt) token = req.cookies.jwt;
   if (!token) {
     return next(
-      new AppErrors('You are not logged in! please log in to get access', 401),
+      new AppError('You are not logged in! please log in to get access', 401),
     );
   }
 
